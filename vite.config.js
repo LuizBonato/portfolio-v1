43a5d1846c1053119ae82c4ef05ce96d99b766b8
Luizbonato <luizeduardo.bonato@outlook.com>
2026-01-19 02:12:42 -0300

    Versão inicial do meu portfólio

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: '/portfolio-v1/',   // <<< LINHA NOVA OBRIGATÓRIA
+  base: '/Porfolio/',   // NOME EXATO DO REPOSITORIO
 })
